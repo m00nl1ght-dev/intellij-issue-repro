cbfdbc2c898ae09345396a4e3c8c249406183ed9
m00nl1ght <elepet1906@gmx.de>
2019-11-21 00:25:06 +0100

    add extra information, add stacktrace.txt

--- src/main/java/SomeSubclass.java
+++ src/main/java/SomeSubclass.java
@@ -7,6 +7,8 @@ public class SomeSubclass<T extends TheWeirdClass<? super T>> extends TestClass<
     void someMethod() {
         List<T> theWeirdListFromSelf = this.theWeirdList; // so this is fine
         List<? super T> theWeirdListFromParent = parent.theWeirdList; // but this breaks it
+        // ^^ changing the type here from explicit to "var" can also more reliably cause the issue
+        // sometimes closing and re-opening the file also helps
 
         // some code here (does not affect the problem)
         System.out.println(theWeirdListFromSelf.toString());
